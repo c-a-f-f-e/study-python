9f5b582e525c96ff93d84fcf39ade1600c658de2
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-09-02 16:43:51 +0200

    メッセージの配置

---
+++ basic/example06-03-01.py
@@ -0,0 +1,12 @@
+#coding:utf-8
+#メッセージと入力欄の配置
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry("400x150")
+root.title("数当てゲーム")
+
+labell = tk.Label(root, text="数を入力してね")
+labell.place(x = 20, y = 20)
+
+root.mainloop()
