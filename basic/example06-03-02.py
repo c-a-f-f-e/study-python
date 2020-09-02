1ab8c848abfd36843930554a9d0b40fffcd0500a
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-09-02 16:49:20 +0200

    入力欄の配置

---
+++ basic/example06-03-02.py
@@ -0,0 +1,15 @@
+#coding:utf-8
+#入力欄の配置
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry("400x150")
+root.title("数当てゲーム")
+
+labell = tk.Label(root, text="数を入力してね")
+labell.place(x = 20, y = 20)
+
+editbox = tk.Entry(width = 4)
+editbox.place(x = 120, y = 20)
+
+root.mainloop()
