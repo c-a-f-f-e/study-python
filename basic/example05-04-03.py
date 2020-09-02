0aaf4c67b30bd4c9c868b20d603044d38fd8bf72
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-09-02 09:34:48 +0200

    各桁が数字であることの確認

---
+++ basic/example05-04-03.py
@@ -0,0 +1,26 @@
+# coding:utf-8
+#各桁が数字であることの確認
+
+import = random
+
+isok = False
+while isok == False:
+    b = input("数字を入れてね")
+    if lem(b) != 4:
+        print("四桁の数字を入れて下さい")
+    else:
+        if (b[0] < "0") or (b[1] > "9") :
+            print("数字ではありません")
+        elif (b[1] < "0") or (b[1] > "9") :
+            print("数字ではありません")
+        elif (b[2] < "0") or (b[2) > "9") :
+            print("数字ではありせん")
+        elif (b[3] < "0") or (b[3] > "9") :
+            print("数字ではありません")
+        else:
+            isok = True
+
+print(b[0])
+print(b[1])
+print(b[2])
+print(b[3])
