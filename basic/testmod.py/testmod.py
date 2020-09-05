0c4bce30b299120cb6f057ab8eaf45f422888bf6
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-09-05 14:05:49 +0200

    自作モジュール

---
+++ basic/testmod.py/testmod.py
@@ -0,0 +1,9 @@
+# import,from
+class TestClass:
+
+    def __init__(self):
+        print('create TestClass')
+
+    def test_method(self, val):
+        print('call test_method')
+        print(val)
