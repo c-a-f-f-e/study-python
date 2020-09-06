5454a136b77bde1ea7076180422f1c61ee840bea
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-09-06 10:57:39 +0200

    python-izm/基礎/可変長引数

--- basic/python_izm02/variadicarguments.py
+++ basic/python_izm02/variadicarguments.py
@@ -5,7 +5,7 @@ def test_func(*args):
 
 test_func(1, 2, 3, 4, 5,)
 
-# 
+# パスの結合
 def test_func(code, name, *args):
     print(code, name)
     print(args)
@@ -13,14 +13,20 @@ def test_func(code, name, *args):
 test_func(100, 'python-izm', 'JP', 'US')
 
 
-# 
+# 引数名は必ずしもargである必要はない
 def test_func(**kwargs):
     print(kwargs)
 
 test_func(code=100, name='python-izm')
 
 
-# 
+# 引数名の前に**でも可変長引数が出来る
+def test_func(**kwargs):
+    print(kwargs)
+test_func(code=100, name='python-izm')
+
+
+# **の可変長引数は関数内においてディクショナリとなる
 def test_func(code, name, kana, *args, **kwargs):
     print(code, name, kana)
     print(args)
