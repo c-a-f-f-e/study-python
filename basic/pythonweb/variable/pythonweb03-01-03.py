1b883659ba719b176e969228ed2381f5170b2342
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-09-07 19:50:19 +0200

    pythonweb/変数/累算代入演算子

---
+++ basic/pythonweb/variable/pythonweb03-01-03.py
@@ -0,0 +1,19 @@
+# coding:utf-8
+num = 10
+print("num = " + str(num))
+
+num += 5
+print("num = " + str(num))
+
+num *= 10
+print("num = " + str(num))
+
+
+
+str = "Hello,"
+str += "Python"
+print(str)
+
+str = "Good!"
+str *= 4
+print(str)
