a59cab494f199009a0386528189dfaf799cefbb0
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-09-07 19:36:18 +0200

    pythonweb/変数/定義と値の代入

---
+++ basic/pythonweb/variable/pythonweb03-01-02.py
@@ -0,0 +1,21 @@
+# coding:utf-8
+personal  = "Yamada"
+print(personal)
+
+personal = "Tarou"
+print(personal)
+
+personal = 24
+print(personal)
+
+
+name1 = "Yamada"
+print("name1=" + name1)
+
+name2 = name1
+print("name1=" + name1)
+print("name2=" + name2 + "\n")
+
+name1 = "Suzuki"
+print("name1=" + name1)
+print("name2=" + name2)
