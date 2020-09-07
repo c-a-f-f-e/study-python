52bcc2dd7c3c1d32d2df92fe4c0138c14bcb766c
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-09-07 20:23:26 +0200

    pythonweb/条件分岐/真偽

---
+++ basic/pythonweb/conditionalbranch/pythonweb04-01-03.py
@@ -0,0 +1,14 @@
+# coding:utf-8
+name = ""
+if name:
+    print("名前は" + name + "です")
+else:
+    print("名前は未登録です")
+
+print("\n")
+
+name = "Yamada"
+if name:
+    print("名前は" + name + "です。")
+else:
+    print("名前は水戸上奥です。")
