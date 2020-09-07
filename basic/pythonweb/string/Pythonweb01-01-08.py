0a65d1896db9ca0288fdb6bfa59ecbc745576c2a
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-09-07 18:18:05 +0200

    pythonweb/文字列/指定したインデックスの取得

---
+++ basic/pythonweb/string/Pythonweb01-01-08.py
@@ -0,0 +1,14 @@
+# coding:utf-8
+str1 = "Hello"
+print(str1[0])
+print(str1[1])
+print(str1[2])
+print(str1[3])
+print(str1[4])
+
+str2 = "こんにちは"
+print(str2[-5])
+print(str2[-4])
+print(str2[-3])
+print(str2[-2])
+print(str2[-1])
