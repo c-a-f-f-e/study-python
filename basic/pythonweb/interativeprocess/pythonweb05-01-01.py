6595271fb7f7d7629e80deea5d9e0465a3e3d7e9
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-09-07 21:13:03 +0200

    pythonweb/繰り返し/while

---
+++ basic/pythonweb/interativeprocess/pythonweb05-01-01.py
@@ -0,0 +1,24 @@
+# coding:utf-8
+num = 1
+print("Start")
+
+while num < 6:
+    print("num = " + str(str(num)))
+    num += 1
+
+print("Emd")
+
+
+num = 1
+total = 0
+print("Start")
+
+while num <6:
+    print("num = " + str(num))
+    total += num
+    num += 1
+else:
+    print("Total = " + str(total))
+
+
+print("End")
