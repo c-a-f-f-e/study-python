ad7126b4e1f5aef7fd1758f6cd418f95ee17542e
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-09-07 18:26:04 +0200

    pythonweb/文字列/スライス

---
+++ basic/pythonweb/string/pythonweb01-01-09.py
@@ -0,0 +1,14 @@
+# coding:utf-8
+str = "Flower"
+print(str[1:4])
+
+str = "Flower"
+print(str[:3])
+print(str[3:])
+print(str[:])
+
+str = "京都府京都市左京区吉田本町"
+print(str)
+print(str[1:12])
+print(str[1:12:3])
+print(str[1:12:3])
