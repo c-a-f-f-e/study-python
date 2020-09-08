a563a0bd866ceb09e373940057e3ede008dbc477
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-09-08 13:55:36 +0200

    pythonweb/繰り返し処理/break文とcontinue文

---
+++ basic/pythonweb/interativeprocess/pythonweb05-01-04.py
@@ -0,0 +1,43 @@
+# coding:utf-8
+num = 0
+while True:
+    print("num = " + str(num))
+    num += 1
+    if num >= 2:
+        break
+
+print("End")
+
+
+
+print("Start")
+
+while True:
+    print("数値を入力すると5倍した値を表示します")
+    print("終了する場合は-1を入力して下さい")
+    num int(input("> "))
+    if num == -1:
+        break
+    print("num = " + str(num) + ",num *" + str(num * 5) + "\n")
+
+print("End")
+
+
+
+for val in [3, "ab", 7]:
+    if isinstance(val,str):
+        continue
+    print("val = " + str(val))
+
+print("End")
+
+
+
+print("Start")
+total = 0
+
+for num in [35, 25, "OK", 45, "Pass", 28]:
+    if isinstans(num, str):
+        continue
+    print("Total = " str(total))
+    print("End")
