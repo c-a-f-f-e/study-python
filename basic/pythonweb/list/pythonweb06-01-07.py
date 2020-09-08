7622bc9a7abb3ce69b056334c672768469c06a23
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-09-08 20:44:19 +0200

    python/リスト/指定した位置への挿入

---
+++ basic/pythonweb/list/pythonweb06-01-07.py
@@ -0,0 +1,21 @@
+# coding:utf-8
+# リストに要素を挿入する
+mylist = ["Blue", "Red", "Green"]
+print(mylist)
+
+mylist.insert(2,"White")
+print(myselist)
+
+mylist.insert(0,"Black")
+print(mylist)
+
+
+# スライス機能を使って挿入する
+mylist = ["Inu", "Uma", "Ashika"]
+print(mylist)
+
+addlist = ["Ushi", "Inu"]
+print(addlist)
+
+mylist[1:1] = addlist
+print(mylist)
