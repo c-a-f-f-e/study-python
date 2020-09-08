a61124da9f1ea602a480cdd85d574223ee7e5cf9
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-09-08 17:33:02 +0200

    pythonweb/リスト/スライス

---
+++ basic/pythonweb/list/pythonweb06-01-03.py
@@ -0,0 +1,14 @@
+# coding:utf-8
+# スライスの範囲指定
+colorlist = ["Blue", "Red", "Green", "White", "Black"]
+
+print(colorlist[1:3])
+print(colorlist[2:3])
+
+
+# 
+colorlist = ["Blue", "Red", "Green", "White", "Black"]
+
+print(colorlist[:3])
+print(colorlist[2:])
+print(colorlist[:])
