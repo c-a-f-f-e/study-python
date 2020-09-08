c3eab5230ed61d1cd0e1eb043c7c9f1d555730a5
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-09-08 13:37:39 +0200

    pythonweb/繰り返し処理/for文

---
+++ basic/pythonweb/interativeprocess/pythonweb05-01-02.py
@@ -0,0 +1,31 @@
+# coding:utf-8
+mylist = ["Orange", "Peach", "Lemon"]
+for val in mylist:
+    print(val)
+
+print("End")
+
+
+
+mylist = ["Orange", "Peach", "Lemon", "Apple"]
+print(mylist)
+for val in mylist:
+    print("value:" + val)
+
+print("\n")
+
+mydict = {"L":"Lemon", "O":"Orange", "G":"Grapes"}
+print(mydict)
+for mykey, myvalue in mydict.items():
+    print("ke:" + mykey + ",value:" + myvalue)
+
+
+
+count = 0
+mylist = ["Orange", "Peach", "Lemon", "Apple"]
+print(mylist)
+for val in mylist:
+    print("value:" + val)
+    count += 1
+else:
+    print("要素の数 = " + str(count))
