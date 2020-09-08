7f71043d3c92b6d3a68890bdc1e12ae262525826
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-09-08 17:56:49 +0200

    pythonweb/リスト/len関数

---
+++ basic/pythonweb/list/python06-01-04.py
@@ -0,0 +1,14 @@
+# coding:utf-8
+# len関数
+print(len(["Red", "Green", "Blue"]))
+
+print(len([1,2,3,4,5]))
+
+print(len([10, 20,["Sun", "Mon", "Fri"], 40]))
+
+
+# サンプルプログラム
+colorlist = ["Blue", "Red", "Green", "White", "Black"]
+
+print(colorlist)
+print("要素数は " + str(len(colorlist)) + "です。")
