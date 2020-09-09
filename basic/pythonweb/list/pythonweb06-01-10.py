5afbb3398f9f5d7ccbf62bc17f0b35d20ca8b102
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-09-09 13:37:51 +0200

    pythonweb/リスト/文字列,タプル,rangeからのリストの作成

---
+++ basic/pythonweb/list/pythonweb06-01-10.py
@@ -0,0 +1,20 @@
+# coding:utf-8
+# 別のリストからリストを作成する
+mylist = ["A", "B", "C"]
+newlist = list(mylist)
+print(newlist)
+
+
+# 文字列からリストを作成する
+mylist = list("Hello Python")
+print(mylist)
+
+
+# タプルからリストを作成する
+mylist = list(("A", "B", "C"))
+print(mylist)
+
+
+# rangeからリストを作成する
+mylist = list(range(10))
+print(mylist)
