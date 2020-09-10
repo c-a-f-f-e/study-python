c3d646400d47497144c1722c9ab48db508981a05
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-09-10 21:59:59 +0200

    pythonweb/集合/集合同士の比較

---
+++ basic/pythonweb/set/pythonweb09-01-05.py
@@ -0,0 +1,44 @@
+# coding:utf-8
+# 集合と他の集合との関係を調べる
+# 等しいかどうか
+set1 = {"A", "B", "C"}
+set2 = {"B", "C", "A"}
+set1 ==  set2
+# setとfrozensetとの比較も可能
+set1 = {"A", "B", "C"}
+set2 = frozenset(["B", "C", "A"])
+set1 == set2
+# 集合が他の集合と互いに素かどうか
+set1 = {"A", "B"}
+set2 = {"B", "D", "C", "A"}
+set3 = {"B", "A"}
+set1 <= set2
+set2 <= set1
+set1 <= set3
+# issubset
+set1 = {"A", "B"}
+set2 = {"B", "D", "C", "A"}
+set3 = {"A", "B"}
+set1.issubset(set2)
+set2.issubset(set1)
+set1.issubset(set3)
+
+
+
+# 集合が集合の真超集合かどうか
+set1 = {"A", "B"}
+set2 = {"B", "D", "C", "A"}
+set3 = {"A", "B"}
+set1 > set2
+set2 > set1
+set1 > set3
+
+
+
+# 集合が他の集合と互いに素かどうか
+set1 = {"A", "B"}
+set2 = {"C", "D", "E"}
+set3 = {"A", "C"}
+set1.isdisjoint(set2)
+set2.isdisjoint(set3)
+set2.isdisjoint(set3)
