6731a26b97688e657b8a2060284cb97c67c2dcf6
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-09-10 12:02:06 +0200

    pythonweb/タプル/インテラブるなオブジェクトからのタプルの作成

---
+++ basic/pythonweb/tuple/pythonweb07-01-08.py
@@ -0,0 +1,20 @@
+# coding:utf-8
+# インテラブるなオブジェクトからタプルを作成する
+# ベルのタプルからタプルを作成する
+mytuple = ("A", "B", "C")
+newtuple = tuple(mytuple);
+print(newtuple)
+
+# 文字列からタプルを作成する
+mytuple = tuple("Hello Python")
+print(mytuple)
+
+#椅子とからタプルを取得する
+mytuple = tuple(["a", "B", "C"])
+print(mytuple)
+
+
+
+# rangeからタプルを取得する
+mytuple = tuple(range(10))
+print(mytuple)
