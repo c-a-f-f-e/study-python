34597370c292d504c87f16084b9691195a0d1eee
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-09-10 19:42:21 +0200

    pythonweb/辞書/指定したキーの有無の確認

---
+++ basic/pythonweb/dictionary/pythonweb08-01-06.py
@@ -0,0 +1,9 @@
+# coding:utf-8
+# 指定のキーの要素が含まれているか確認
+mydict = {"L":"Lemon", "O":"Orange", "G":"Grapes"}
+print("O" in mydict)
+print("P" in mydict)
+
+mydict = {"L":"Lemon", "O":"Orange", "G":"Grapes"}
+print("O" not in mydict)
+print("P" not in mydict)
