39cd611ecc26ce908195518e3f1ecea413dc93b8
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-09-10 13:27:28 +0200

    pythonweb/辞書/辞書の作成

---
+++ basic/pythonweb/dictionary/pythonweb08-01-01.py
@@ -0,0 +1,17 @@
+# coding:utf-8
+# 辞書の作成
+# 変数に代入された値を要素として指定
+x = 1
+y = "Desktop"
+mydict = {x:y}
+print(mydict)
+
+mydict = {1:"Desktop"}
+print(mydict)
+
+x = 1
+y = "Desktop"
+mydict = {x:y}
+print(mydict)
+y = "Movie"
+print(mydict)
