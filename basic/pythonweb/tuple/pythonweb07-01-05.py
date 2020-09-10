420fc0caae8abc17dd95419527255ae24fd0a808
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-09-10 11:28:08 +0200

    python/タプル/タプルの結合

---
+++ basic/pythonweb/tuple/pythonweb07-01-05.py
@@ -0,0 +1,9 @@
+# coding:utf-8
+# 別のタプルと結合して新しいタプルを作成
+redtuple = ("Orange", "Strawberry")
+print(redtuple)
+yellowtuple = ("Lemon", "Banana", "Grapefruit")
+print(yellowtuple)
+# redtupleとyellowtupleの結合
+fruittuple = redtuple + yellowtuple
+print(fruittuple)
