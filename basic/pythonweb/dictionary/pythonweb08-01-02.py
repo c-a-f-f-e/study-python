9cb42c942450689a76716b6e2f10b7e80853b6b6
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-09-10 13:47:40 +0200

    pythonweb/辞書/キーを指定し手の値の取得

---
+++ basic/pythonweb/dictionary/pythonweb08-01-02.py
@@ -0,0 +1,26 @@
+# coding:utf-8
+# 辞書でキーを指定して値を取得
+mydict = {"JP":"Japan", "DE":"Germany", "FR":"France"}
+print(mydict["DE"])
+print(mydict["FR"])
+print(mydict["JP"])
+
+mydict = {"JP":"Japan", "DE":"Germany", "FR":"France"}
+
+print(mydict)
+key1 = "JP"
+print("キー:" + key1 + "、値:" + mydict[key1])
+key2 = "FR"
+print("キー:" + key2 + "、値:" + mydict[key2])
+
+
+# getメソッドを使用いsて値を取得する
+mydict = {"JP":"Japan", "DE":"Germany", "FR":"France"}
+
+print(mydict)
+key1 = "JP"
+print("キー:" + key1 + "、値:" + mydict.get(key1, "NotFound"))
+key2 = "FR"
+print("キー:" + key2 + "、値:" + mydict.get(key2, "NotFound"))
+key3 = "EN"
+print("キー:" + key3 + "、値:" + mydict.get(key3, "NotFound"))
