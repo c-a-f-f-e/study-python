9807e85ae822374e3e7abbbd08beb52102357600
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-09-11 18:54:05 +0200

    pythonweb/print関数/基本と応用

---
+++ basic/dotblog/print_function/print01.py
@@ -0,0 +1,10 @@
+#coding:utf-8
+# 基本
+print('ここに出力したい内容が入ります')
+print("ここに出力したい'内容'が入ります")
+
+# 応用
+print('python', 'プログラム', 'print関数')
+print('Python' + 'プログラム' + 'print関数')
+print('python' * 3)
+print('python','プログラム'　+ 'print関数' * 2)
