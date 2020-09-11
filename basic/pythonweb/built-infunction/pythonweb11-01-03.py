ebf64bb5e638d68dfe793697bdc62738068ab2be
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-09-11 14:12:34 +0200

    pythonweb/組み込み関数/bool関数

---
+++ basic/pythonweb/built-infunction/pythonweb11-01-03.py
@@ -0,0 +1,13 @@
+# coding:utf-8
+# int関数/float関数の使い方
+# int関数の基本的な使い方
+print(int(17.58))
+print(int("1011",2))
+print(int("A4", base=16))
+
+
+
+# float関数の書式と基本的なかき方
+print(float(19))
+print(float("78.225"))
+print(float("-7.52e-3"))
---
+++ basic/pythonweb/built-infunction/pythonweb11-01-05.py
@@ -0,0 +1,7 @@
+# coding:utf-8
+# bool関数の使い方
+#bool関数の書式と簡単な書き方
+print(bool("Hello"))
+print(bool(""))
+print(bool(0))
+print(bool(0.7))
